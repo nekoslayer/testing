b00a398532515c4781ce796eca1170aa7e80b458
GÖRKEM YILDIRIM <190722066@stu.istinye.edu.tr>
2022-08-29 18:24:46 +0300

    Automatic save commit initiated at Mon, Aug 29, 2022  6:24:46 PM

---
+++ activate33.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+### project 1 ###
+cd testing/
+git add .
+git commit -m  "Automatic save commit initiated at $(date)"
+git push origin master
--- htmleditingwithpy.py
+++ htmleditingwithpy.py
@@ -15,32 +15,6 @@ Func.write("""<head>\n <title>testsite</title>\n <meta name="description" conten
 Func.close()
 
 
-## sende git path'i kurulu olmadığı için bu alttaki kısım çalışmayabilir html'i local açsan da olur
-## bu kısım hem linux hem windowsta çalışıyor %100
-##time.sleep(5)
-##
-##
-##GITHUB_TOKEN = "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"
-##API_URL ="https://api.github.com"
-##payload = '{"name": "Testname"}'
-##headers= {
-##             "Authorization": "token " + GITHUB_TOKEN,
-##             "Accept": "application/vnd.github.v3.json"
-##}
-##
-##
-##
-##r = requests.post(API_URL+"/user/repos", data=payload, headers=headers)
-##
-##
-##print(r.json())
-
-
-##time.sleep(1)
-##os.system(curl -i -u nekoslayer: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 https://api.github.com/users/nekoslayer)
-##
-
-
 
 print("done")
 input()
